cb0bad4e709567cfd3a3eec24aec9f149303f4a5
Liam Abbott <liam.e.abbott@gmail.com>
2017-09-20 14:49:07 -0400

    updated connect

--- compute/connect.py
+++ compute/connect.py
@@ -1,10 +1,10 @@
 import os
-from subprocess import Popen, check_call
+from subprocess import Popen, check_call, call
 
 def init_parser(parser):
     parser.add_argument('name', type=str, help='Cluster name.')
-    parser.add_argument('service', type=str,
-                        choices=['r', 'rstudio'],
+    parser.add_argument('service', type=str, nargs='?', default='ssh',
+                        choices=['ssh', 'r', 'rstudio'],
                         help='Web service to launch.')
     parser.add_argument('--port', '-p', default='10000', type=str,
                         help='Local port to use for SSH tunnel to master node (default: %(default)s).')
@@ -25,32 +25,47 @@ def main(args):
 
     # Dataproc port mapping
     compute_ports = {
-        'rstudio': 8787
+        'rstudio': 8787,
+        'ssh': 22
     }
     connect_port = compute_ports[service]
 
-    # open SSH tunnel to master node
-    cmd = [
-        'gcloud',
-        'compute',
-        'ssh',
-        '{}'.format(args.name),
-        '--zone={}'.format(args.zone),
-        '--ssh-flag=-D {}'.format(args.port),
-        '--ssh-flag=-N',
-        '--ssh-flag=-f',
-        '--ssh-flag=-n'
-    ]
-    with open(os.devnull, 'w') as f:
-        check_call(cmd, stdout=f, stderr=f)
+    if service != 'ssh':
 
-    # open Chrome with SOCKS proxy configuration
-    cmd = [
-        r'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-        'http://localhost:{}'.format(connect_port),
-        '--proxy-server=socks5://localhost:{}'.format(args.port),
-        '--host-resolver-rules=MAP * 0.0.0.0 , EXCLUDE localhost',
-        '--user-data-dir=/tmp/'
-    ]
-    with open(os.devnull, 'w') as f:
-        Popen(cmd, stdout=f, stderr=f)
+        # open SSH tunnel to master node
+        cmd = [
+            'gcloud',
+            'compute',
+            'ssh',
+            '{}'.format(args.name),
+            '--zone={}'.format(args.zone),
+            '--ssh-flag=-D {}'.format(args.port),
+            '--ssh-flag=-N',
+            '--ssh-flag=-f',
+            '--ssh-flag=-n'
+        ]
+        with open(os.devnull, 'w') as f:
+            check_call(cmd, stdout=f, stderr=f)
+
+        # open Chrome with SOCKS proxy configuration
+        cmd = [
+            r'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+            'http://localhost:{}'.format(connect_port),
+            '--proxy-server=socks5://localhost:{}'.format(args.port),
+            '--host-resolver-rules=MAP * 0.0.0.0 , EXCLUDE localhost',
+            '--user-data-dir=/tmp/'
+        ]
+        with open(os.devnull, 'w') as f:
+            Popen(cmd, stdout=f, stderr=f)
+
+    elif service == 'ssh':
+
+        # open SSH tunnel to master node
+        cmd = [
+            'gcloud',
+            'compute',
+            'ssh',
+            '{}'.format(args.name),
+            '--zone={}'.format(args.zone)
+        ]
+        call(cmd)
